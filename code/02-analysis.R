f09bcb5757ecffe553ca0f30e4773b1ae435f9c5
Jorge Cimentada <cimentadaj@gmail.com>
2019-10-04 18:15:37 +0200

    Adds and runs all drake targets up to escs_dummy

--- code/02-analysis.R
+++ code/02-analysis.R
@@ -1,5 +1,5 @@
+read_harmonize_pisa <- function(raw_data, recode_cntrys) {
 
-read_harmonize_pisa <- function(raw_data) {
   # Read PISA 2012 files
   pisa2012_path <- file.path(raw_data, "pisa2012", "INT_STU12_DEC03.txt")
   dic_path <- file.path(raw_data, "pisa2012", "PISA2012_SAS_student.sas")
@@ -10,17 +10,29 @@ read_harmonize_pisa <- function(raw_data) {
 
   colnames(student2012) <- dic_student$varname
 
+  print("pisa2012 read")
+
   # Read PISA 2015 data
   pisa2015_path <- file.path(raw_data, "pisa2015", "CY6_MS_CMB_STU_QQQ.sav")
   pisa_countrynames <- c(pisa_countrynames, "United States" = "USA")
   student2015 <- read_spss(pisa2015_path)
+
+  print("pisa2015 read")
 
   # Provisional code for separate PISA2012
   # Vector with database names except student2015
   databases <- c("math2000", paste0("student", seq(2003, 2009, 3)))
 
   # Create a list with all data sets
-  pisa_list <- map(c(databases, paste0("student", c(2012, 2015))), get)
+  pisa_list <-
+    list(
+      student2000 = math2000,
+      student2003 = student2003,
+      student2006 = student2006,
+      student2009 = student2009,
+      student2012 = student2012,
+      student2015 = student2015
+    )
 
   # Give each dataset their own name
   names(pisa_list) <- c("student2000",
@@ -33,6 +45,19 @@ read_harmonize_pisa <- function(raw_data) {
 
   # Create a data frame with a list column containing all lists.
   pisa_all <- enframe(pisa_list)
+
+  print("pisa_all ready")
+
+  # Remove everything that uses memory
+  rm(pisa_list,
+     dic_student,
+     student2012,
+     student2015)
+
+  unloadNamespace("PISA2000lite")
+  unloadNamespace("PISA2003lite")
+  unloadNamespace("PISA2006lite")
+  unloadNamespace("PISA2009lite")
 
   # Go through each PISA and grab the highest education from Male
   # and Female partner. Finally, recode it to three categories
@@ -73,10 +98,70 @@ read_harmonize_pisa <- function(raw_data) {
       as_tibble()
   })
 
+  years <- seq(2000, 2015, 3)
+
+  db <- paste0("pisa", years)
+  pisa_all$value <- map2(pisa_all$value, db, ~ { .x$wave <- .y; .x})
+  pisa_all$value[[1]]$CNT <- pisa_all$value[[1]]$COUNTRY
+
+  print("Before going into valueeee")
+  pisa_all$value <-
+    map(pisa_all$value, ~ {
+      # 2000 to 2015
+      # The coding is from 0 to 6, where 0 is no schooling and 6 is
+      # BA or above.
+
+      # When turning 0:6 to numeric, it becomes 1:7 that's why
+      # I recode 8:9 to NA. This, however, did not work for last two surveys
+
+      .x$father_edu <- car::recode(as.numeric(.x$FISCED), "8:9 = NA")
+      .x$mother_edu <- car::recode(as.numeric(.x$MISCED), "8:9 = NA")
+      .x$high_edu_broad <- pmax(.x$father_edu, .x$mother_edu)
+      .x$country <- car::recode(.x$CNT, recode_cntrys)
+
+
+      if (any(unique(.x$wave) %in% c("pisa2012", "pisa2015"))) {
+        # These two surveys were from 0:6 so I had to add + 1
+        # so that it equals 1:7 as all other surveys.
+        .x$father_edu <- .x$father_edu + 1
+        .x$mother_edu <- .x$mother_edu + 1
+        .x$high_edu_broad <- .x$high_edu_broad + 1
+      }
+
+      # PISA 2015 doesn't have the schoolid column at the moment.
+      if (unique(.x$wave) == "pisa2015") {
+        select(.x,
+               wave,
+               country,
+               AGE,
+               matches("PV[0-9]{1,2}[MATH|READ]"),
+               W_FSTUWT,
+               ESCS,
+               high_edu_broad,
+               gender,
+               matches("ST16Q03|ST37Q01|ST019CQ01T|ST013Q01TA"))
+      } else {
+        select(.x,
+               wave,
+               country,
+               SCHOOLID,
+               STIDSTD,
+               AGE,
+               matches("PV[0-9]{1,2}[MATH|READ]"),
+               W_FSTUWT,
+               high_edu_broad,
+               gender,
+               matches("ST16Q03|ST37Q01|ST019CQ01T|ST013Q01TA"))
+      }
+
+    })
+
+
   pisa_all
+
 }
 
-read_escs <- function(raw_data) {
+read_escs <- function(raw_data, recode_cntrys) {
   escs_path <- file.path(raw_data, "escs_data")
   escs_files <- list.files(escs_path, pattern = ".sav", full.names = TRUE)
   escs_trend <- map(escs_files, haven::read_spss)
@@ -89,4 +174,59 @@ read_escs <- function(raw_data) {
 
   escs_trend
 }
+
+merge_data <- function(pisa_all, escs_trend) {
+  pisa_all <- as_tibble(pisa_all)
+  # Next we'll merge the ESCS data with the PISA data. As explained above, the 6th data (PISA
+  # 2015) does not need to be merged so I exclude it with this vector
+  exclude <- -6
+  years <- seq(2000, 2015, 3)
+
+  # Loop in parallel to the PISA data, the ESCS data and the year vector (which is seq(2012, 2015, 3))
+  pisa_all$value[exclude] <-
+    pmap(list(pisa_all$value[exclude], escs_trend, years[exclude]), function(.x, .y, .z) {
+
+      # The escs data needs to have the key variables the same class as the
+      # same data.
+      escs <-
+        .y %>% mutate(schoolid = as.numeric(schoolid),
+                      stidstd = as.numeric(stidstd))
+
+      # .z is the corresponding year that will be created as a column
+      # And perform the same transformation of the key variables as in the ESCS data
+      data_trend <-
+        .x %>%
+        mutate(
+          year = .z,
+          schoolid = as.numeric(as.character(SCHOOLID)),
+          stidstd = as.numeric(as.character(STIDSTD))
+        ) %>%
+        left_join(escs,
+                  by = c("country", "schoolid", "stidstd"))
+
+      message(paste(unique(.x$wave), "done"))
+
+      data_trend
+    })
+
+  pisa_all$value[[6]] <-
+    pisa_all$value[[6]] %>%
+    rename(escs_trend = ESCS)
+
+  adapted_year_data <-
+    map(pisa_all$value, ~ {
+      if (unique(.x$wave) == "pisa2000") {
+        # pisa2000 has a different coding so here I recode 6 to 7 so that in all
+        # waves the top edu is 7 and the bottom is 1
+        .x <-
+          mutate(.x, new_hisced = as.character(dplyr::recode(as.numeric(high_edu_broad), `6` = 7)))
+      } else {
+        .x <-
+          mutate(.x, new_hisced = as.character(high_edu_broad))
+      }
+
+      .x
+    })
+
+  adapted_year_data
 }
--- code/plan.R
+++ code/plan.R
@@ -1,3 +1,5 @@
+############################# Config values ###################################
+###############################################################################
 Sys.setenv(R_MAX_NUM_DLLS = 512)
 
 raw_data_dir <- here("raw_data")
@@ -51,8 +53,217 @@ reliability_pisa <-
     "2012" = 0.82,
     "2015" = 0.74) # 2015 imputed
 
+
+# Create recoding vector for car::recode in the below map call.
+recode_cntrys <-
+  enframe(pisa_countrynames) %>% # names come from personal cimentadaj package
+  mutate(recoder = paste0("'", name, "'", " = ", "'", value, "'")) %>%
+  pull(recoder) %>%
+  paste0(collapse = ";")
+
+
+############################# Helper functions ################################
+###############################################################################
+
+
+# Function calculates the bottom 30th quantile for the bottom educated and the 70th quantile
+# for the top educated. If the quantiles cannot be estimated, it returns two NA's instead
+quantile_missing <- function(df, weights, probs) {
+
+  quan <- try(Hmisc::wtd.quantile(
+    df$escs_trend,
+    weights = df[[weights]],
+    probs = probs
+  ))
+
+  if (any("try-error" %in% class(quan))) {
+    return(c(NA, NA))
+  } else {
+    return(c(quan[1], quan[2]))
+  }
+}
+
+# Producing the plot to get the difference between the top 30% of the high educated
+# vs the bottom 30% of the low educated. This function loops through each dataset/country
+# and survey reliability and estimates the difference while also extracting the s.e. of each
+# difference.
+
+# .x <- adapted_year_data[[1]]
+# .y <- reliability_pisa[1]
+# test <- "MATH"
+# probs <- c(0.1, 0.9)
+
+# It returns a dataframe for each survey with all countries and respective coefficients and
+# standard errors.
+test_diff <- function(df, reliability, test, probs) {
+
+  map2(df, reliability, function(.x, .y) {
+
+    conf <- if (unique(.x$wave) == "pisa2015") pisa2015_conf else pisa_conf
+    weights_var <- conf$variables$weightFinal
+
+    country_split <- split(.x, .x$country)
+    
+    country_list <- map(country_split, function(country) {
+      print(unique(country$country))
+
+      quan <- quantile_missing(country, weights_var, probs)
+
+      # it is very important to create a variable that returns the number of observations of this dummy
+      # For each country. Possibly to weight by the number of observations.
+      country$escs_dummy <-
+        with(country, case_when(escs_trend >= quan[2] ~ 1,
+                                escs_trend <= quan[1] ~ 0))
+      country
+    })
+    
+    rm(country_split)
+
+    .x <-
+      enframe(country_list) %>%
+      unnest(value)
+    
+    rm(country_list)
+
+    .x <-
+      .x %>%
+      dplyr::select(wave,
+                    matches(paste0("^PV.*", test, "$")),
+                    escs_dummy,
+                    country,
+                    one_of(weights_var),
+                    AGE)
+
+    message(paste(unique(.x$wave), "data ready"))
+
+
+    test_vars <- paste0("PV", seq_len(conf$parameters$PVreps), test)
+    .x[test_vars] <- map(.x[test_vars], ~ ifelse(.x == 9997, NA, .x))
+
+    # Calculate median math score of all PV's
+    .x$dv <- rowMedians(as.matrix(.x[test_vars]), na.rm = T)
+
+    # Should I estimate the model separately by country?
+    mod1 <- lm(dv ~ AGE,
+               weights = .x[[weights_var]],
+               data = .x,
+               na.action = "na.exclude")
+
+    # Take residuals of model and divide by rmse. Multiply that by
+    # 1 / sqrt(reliability of each survey), which is .y in the loop.
+    .x$adj_pvnum <- resid(mod1)/modelr::rmse(mod1, .x) * 1 / sqrt(.y)
+
+    mod2 <-
+      lmer(adj_pvnum ~ escs_dummy + (1 + escs_dummy | country),
+           data = .x,
+           weights = .x[[weights_var]])
+
+    # Take the country coefficients (absolute coefficients)
+    country_coef <-
+      coef(mod2)$country %>%
+               rownames_to_column() %>%
+               gather(escs_dummy, Mean, -rowname) %>%
+               mutate(escs_dummy = dplyr::recode(escs_dummy,
+                                                 `(Intercept)` = "0",
+                                                 `escs_dummy` = "1"))
+
+    # Take the absolute country standard errors
+    se <-
+      se.coef(mod2)$country %>%
+                  as.data.frame() %>%
+                  rownames_to_column() %>%
+                  gather(escs_dummy, s.e., -rowname) %>%
+                  mutate(escs_dummy = dplyr::recode(escs_dummy,
+                                                    `(Intercept)` = "0",
+                                                    `escs_dummy` = "1"))
+
+    results <-
+      inner_join(country_coef, se, by = c("rowname", "escs_dummy")) %>%
+      rename(country = rowname) %>%
+      arrange(country, escs_dummy)
+
+    message(paste0(unique(.x$wave), " modeling done"))
+    list(results, mod2)
+  })
+}
+
+
+# Adapted from: https://github.com/jtleek/slipper/blob/master/R/slipper.R
+# Returns a tibble with the actual expr + the bootstrapped expr.
+bootstrapper <- function(df, expr, B = 100, n = nrow(df), replacement = TRUE) {
+  bootstrapper_(df, lazyeval::lazy(expr), B, n, replacement)
+}
+
+bootstrapper_ <- function(df, expr, B = 500, n = nrow(df), replacement = TRUE) {
+  obs_val = lazyeval::lazy_eval(expr, data = df)
+  boot_val = replicate(B, {
+    newdata = sample_n(df, n, replace = replacement)
+    lazyeval::lazy_eval(expr, data = newdata)
+  })
+  out = tibble(type = c("observed", "bootstrap"),
+               value = c(obs_val, mean(boot_val, na.rm = T)))
+  return(out)
+}
+
+# For example
+# bootstrapper(mtcars, mean(mpg), B = 200)
+
+f_ind <- function(.x) .x[[1]]
+
+enframer <- function(df, col_name = "name") {
+  df %>%
+    enframe(name = col_name) %>%
+    unnest()
+}
+
+escs_dummy_creator <- function(df, probs) {
+
+  map(df, function(.x) {
+
+    conf <- if (unique(.x$wave) == "pisa2015") pisa2015_conf else pisa_conf
+    weights_var <- conf$variables$weightFinal
+
+    country_split <- split(.x, .x$country)
+
+    country_list <- map(country_split, function(country) {
+      print(unique(country$country))
+
+      quan <- quantile_missing(country, weights_var, probs)
+
+      # it is very important to create a variable that returns the number of observations of this dummy
+      # For each country. Possibly to weight by the number of observations.
+      country$escs_dummy <-
+        with(country, case_when(escs_trend >= quan[2] ~ 1,
+                                escs_trend <= quan[1] ~ 0))
+      country
+    })
+    rm(country_split)
+
+    .x <-
+      enframe(country_list) %>%
+      unnest(value)
+    
+    rm(country_list)
+
+    message(paste(unique(.x$wave), "data ready"))
+
+    .x
+  })
+}
+
+############################# Drake plan ######################################
+###############################################################################
+
 plan <-
   drake_plan(
-    pisa_data = target(read_harmonize_pisa(raw_data_dir), format = "fst"),
-    escs_data = read_escs(raw_data_dir),
+    pisa_data = target(
+      read_harmonize_pisa(raw_data_dir, recode_cntrys),
+      format = "fst"
+    ),
+    escs_data = read_escs(raw_data_dir, recode_cntrys),
+    merged_data = merge_data(pisa_data, escs_data),
+    res_math = test_diff(merged_data, reliability_pisa, "MATH", c(0.1, 0.9)),
+    res_read = test_diff(merged_data, reliability_pisa, "READ", c(0.1, 0.9))
+    ## results_math = map(res_math, f_ind),
+    ## results_read = map(res_read, f_ind)
   )
