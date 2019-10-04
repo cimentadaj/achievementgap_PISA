a113fdae947ac344394d98f1c3034d343d26ab85
Jorge Cimentada <cimentadaj@gmail.com>
2019-10-04 15:26:20 +0200

    Adds read_pisa + read_escs in drake plan

--- code/02-analysis.R
+++ code/02-analysis.R
@@ -1,7 +1,92 @@
-read_pisa <- function(file_path, save_path) {
-  mtcars
+
+read_harmonize_pisa <- function(raw_data) {
+  # Read PISA 2012 files
+  pisa2012_path <- file.path(raw_data, "pisa2012", "INT_STU12_DEC03.txt")
+  dic_path <- file.path(raw_data, "pisa2012", "PISA2012_SAS_student.sas")
+  dic_student <- parse.SAScii(sas_ri = dic_path)
+  student2012 <- read_fwf(pisa2012_path,
+                          col_positions = fwf_widths(dic_student$width),
+                          progress = TRUE)
+
+  colnames(student2012) <- dic_student$varname
+
+  # Read PISA 2015 data
+  pisa2015_path <- file.path(raw_data, "pisa2015", "CY6_MS_CMB_STU_QQQ.sav")
+  pisa_countrynames <- c(pisa_countrynames, "United States" = "USA")
+  student2015 <- read_spss(pisa2015_path)
+
+  # Provisional code for separate PISA2012
+  # Vector with database names except student2015
+  databases <- c("math2000", paste0("student", seq(2003, 2009, 3)))
+
+  # Create a list with all data sets
+  pisa_list <- map(c(databases, paste0("student", c(2012, 2015))), get)
+
+  # Give each dataset their own name
+  names(pisa_list) <- c("student2000",
+                        databases[-1],
+                        "student2012",
+                        "student2015")
+
+  # Fix the country variable name which is different for PISA 2000
+  pisa_list$math2000$CNT <- pisa_list$math2000$COUNTRY
+
+  # Create a data frame with a list column containing all lists.
+  pisa_all <- enframe(pisa_list)
+
+  # Go through each PISA and grab the highest education from Male
+  # and Female partner. Finally, recode it to three categories
+  pisa_all$value <- map(pisa_all$value, function(each_pisa) {
+    each_pisa$high_edu <- pmax(as.numeric(each_pisa$MISCED), # find highest education
+                               as.numeric(each_pisa$FISCED)) # in the household
+    
+    # Recode new highest education into three categories
+    each_pisa$high_edu <- recode(each_pisa$high_edu, "1:3 = 1; 4:5 = 2; 6:7 = 3; else = NA")
+    each_pisa
+  })
+
+  # Gender variables:
+  # 2000: ST03Q01
+  # 2003: ST03Q01
+  # 2006: ST04Q01
+  # 2009: ST04Q01
+  # 2012: ST04Q01
+  # 2015: ST004D01T
+
+  # Vector with all gender vars in order of PISA surveys
+  gender <- c("ST03Q01",
+              "ST03Q01",
+              "ST04Q01",
+              "ST04Q01",
+              "ST04Q01",
+              "ST004D01T")
+
+  # Loop through each data frame and assign the gender variable name
+  # and turn it into character
+  pisa_all$value <- map(seq_along(pisa_all$value), function(pisa_data) {
+    
+    pisa_all$value[[pisa_data]] %>%
+      rename_("gender" = gender[pisa_data]) %>%
+      mutate(gender = as.character(gender),
+             country = pisa_countrynames[as.character(CNT)],
+             region = countrycode(country, "country.name", "region")) %>%
+      as_tibble()
+  })
+
+  pisa_all
 }
 
-select_few <- function(read_data) {
-  select(read_data, mpg, cyl)
+read_escs <- function(raw_data) {
+  escs_path <- file.path(raw_data, "escs_data")
+  escs_files <- list.files(escs_path, pattern = ".sav", full.names = TRUE)
+  escs_trend <- map(escs_files, haven::read_spss)
+
+  escs_trend <-
+    map(escs_trend, ~ {
+    .x$country <- car::recode(.x$cnt, recode_cntrys)
+    .x
+  })
+
+  escs_trend
+}
 }
--- code/plan.R
+++ code/plan.R
@@ -1,5 +1,58 @@
+Sys.setenv(R_MAX_NUM_DLLS = 512)
+
+raw_data_dir <- here("raw_data")
+
+pisa2015_conf <- list(
+  variables = list(pvlabelpref = "PV",
+                   pvlabelsuff = "READ",
+                   weightFinal = "W_FSTUWT",
+                   weightBRR = "W_FSTURWT"),
+  parameters = list(cutoffs = c(357.77, 420.07, 482.38, 544.68, 606.99, 669.30),
+                    percentiles = c(5, 10, 25, 75, 90, 95),
+                    PVreps = 10,
+                    BRRreps = 80,
+                    weights = "BRR",
+                    replication_scheme = 'pisa')
+)
+
+
+countries <- c("Finland",
+               "France",
+               # "New Zealand",
+               "Austria",
+               "Australia",
+               "Sweden",
+               # "Czech Republic",
+               "Canada",
+               "Hungary",
+               # "Iceland",
+               "Netherlands",
+               "Spain",
+               # "Belgium",
+               "Italy",
+               # "Norway",
+               "United Kingdom",
+               # "Greece",
+               "Denmark",
+               # "Israel",
+               "Poland",
+               "United States",
+               "Germany"
+               # "Turkey",
+               # "Russia"
+               )
+
+
+reliability_pisa <-
+  c("2000" = 0.81,
+    "2003" = 0.85,
+    "2006" = 0.78,
+    "2009" = 0.74,
+    "2012" = 0.82,
+    "2015" = 0.74) # 2015 imputed
+
 plan <-
   drake_plan(
-    read_data = read_pisa(),
-    process_data = select_few(read_data),
+    pisa_data = target(read_harmonize_pisa(raw_data_dir), format = "fst"),
+    escs_data = read_escs(raw_data_dir),
   )
